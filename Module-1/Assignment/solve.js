cb4a89e23f85e19dd3eef218dedd5725f4127165
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-03-22 13:23:10 +0600

    Assignment Done

---
+++ Module-1/Assignment/solve.js
@@ -0,0 +1,10 @@
+// Question: Write a program that generates a multiplication table for a given number using a for loop.
+
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    let product = num * i;
+    console.log(`${num} X ${i} = ${product} `);
+  }
+}
+
+multiplicationTable(10);
