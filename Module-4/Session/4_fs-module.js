898f3245a7d09045d9d4966c26f27e5099093c18
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-04-19 19:52:12 +0600

    Node JS

--- Module-4/Session/4_fs-module.js
+++ Module-4/Session/4_fs-module.js
@@ -1,15 +1,50 @@
 const fs = require('fs');
 const http = require('http');
-const port = 4000;
+const port = 5050;
 
 const server = http.createServer((req, res) => {
+  // :::::: 1. readFile() ::::::
+  // if (req.url === '/') {
+  //   fs.readFile('index.html', (error, data) => {
+  //     res.writeHead(200, { 'Content-Type': 'text/html' });
+  //     res.write(data);
+  //     res.end();
+  //   });
+  // }
+  // :::::: 1. readFileSync() ::::::
+  // if (req.url === '/') {
+  //   let data = fs.readFileSync('index.html');
+  //   res.writeHead(200, { 'Context-Type': 'text/html' });
+  //   res.write(data);
+  //   res.end();
+  // }
+  // :::::: 2. writeFile() ::::::
+  // if (req.url === '/') {
+  //   fs.writeFile('test.txt', 'Hello, Node JS', (error) => {
+  //     if (error) {
+  //       res.writeHead(400, { 'Context-Type': 'text/html' });
+  //       res.write('Something is Wrong');
+  //       res.end();
+  //     } else {
+  //       res.writeHead(200, { 'Content-Type': 'text/html' });
+  //       res.write('File Write Successfully...');
+  //       res.end();
+  //     }
+  //   });
+  // }
+  // :::::: 2. writeFileSync() ::::::
   if (req.url === '/') {
-    // Async
-    fs.readFile('index.html', (error, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
+    let error = fs.writeFileSync('demo.txt', 'Welcome, Node JS');
+
+    if (error) {
+      res.writeHead(400, { 'Context-Type': 'text/html' });
+      res.write('Something is Wrong');
       res.end();
-    });
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('File Write Successfully...');
+      res.end();
+    }
   }
 });
 
