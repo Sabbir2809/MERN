e85c03ab0e407e9ace269af582d1b00d9715ff02
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-08-04 15:08:36 +0600

    React-Redux Counter Apps

--- Module-13/Tutorail/react-redux-apps/redux/slice/counterSlice.js
+++ Module-13/Tutorail/react-redux-apps/redux/slice/counterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const counterSlice = createSlice({
+export const counterSlice =  createSlice({
   name: 'counter',
   initialState: {
     value: 0,
