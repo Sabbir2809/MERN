3975276698e10fa4a8b6f1c8e33c5daf62e4c2c1
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-08-20 23:15:45 +0600

    Backend Development: server environment setup & Introduction to npm packages, connect Mongodb with express js, connect MongoDB  and Create user-login API, verify-login API, get product-list API, JWT, mongoose schema, Postman

--- Module-15/mini-ecommerce/back-end/src/routes/api.js
+++ Module-15/mini-ecommerce/back-end/src/routes/api.js
@@ -2,17 +2,24 @@
 const express = require("express");
 const { register, login } = require("../controllers/userController");
 const { createProduct, getAllProducts } = require("../controllers/productController");
+const { createCart, getAllCartList, deleteCart } = require("../controllers/cartController");
+const { authVerify } = require("../middleware/authVerifyMiddleware");
 const router = express.Router();
 
 // API Routing End Point:
 
-// @User Authentication: register, login
+// @User Authentication API: register, login
 router.post("/auth/user-register", register);
 router.get("/auth/user-login", login);
 
-// @Product: createProduct, getAllProducts
-router.post("/product/create-product", createProduct);
-router.get("/product/get-products", getAllProducts);
+// @Product API: createProduct, getAllProducts
+router.post("/create-product", authVerify, createProduct);
+router.get("/products", authVerify, getAllProducts);
+
+// @Cart API: createCar, getAllCartList, deleteCart
+router.post("/create-cart", authVerify, createCart);
+router.post("/cart-list", authVerify, getAllCartList);
+router.delete("/delete-cart/:id", authVerify, deleteCart);
 
 // Exports
 module.exports = router;
